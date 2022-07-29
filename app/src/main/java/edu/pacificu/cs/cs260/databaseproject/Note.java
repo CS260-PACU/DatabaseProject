e5cb71afab560ef08994773ad470b11c57f16375
chadd williams <will4614@pacificu.edu>
2022-07-29 11:55:23 -0700

    Initial Commit, not finished

---
+++ app/src/main/java/edu/pacificu/cs/cs260/databaseproject/Note.java
@@ -0,0 +1,26 @@
+package edu.pacificu.cs.cs260.databaseproject;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Note {
+    @PrimaryKey
+    public int nid;
+
+    @ColumnInfo(name = "msg")
+    public String msg;
+
+    @ColumnInfo(name="datetime")
+    public String datetime;
+
+    @ColumnInfo(name="gps_lat")
+    public double gps_Lat;
+
+    @ColumnInfo(name="gps_lon")
+    public double gps_Lon;
+
+    @ColumnInfo(name = "image")
+    public byte[] image;
+}
---
+++ app/src/main/java/edu/pacificu/cs/cs260/databaseproject/NoteDao.java
@@ -0,0 +1,26 @@
+package edu.pacificu.cs.cs260.databaseproject;
+
+import androidx.room.Delete;
+import androidx.room.Index;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+public interface NoteDao {
+
+    @Query("Select * FROM note")
+    List<Note> getAll();
+
+    @Query("SELECT * From note WHERE nid = :noteid")
+    Note findByID(int noteid);
+
+    @Insert
+    void insertAll(Note... notes);
+
+    @Delete
+    void delete(Note note);
+
+
+
+}
